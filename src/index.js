487a35e9ab32b3e77d1aa3a20e19ed7e24e869ce
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2019-05-17 19:14:15 +0100

    throw  when required arguments are missing

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@
 
 const fs = require('fs');
 const html2pug = require('html2pug');
-const fixTabs = require('./src/fixTabs');
+const fixTabs = require('./fixTabs');
 
-function convert (options = {src: './html-snippets.json', dist: './pug-snippets.json'}) {
-  let htmlSnippets = fs.readFileSync(options.src, {encoding: 'utf-8'})
-  let pugSnippets = JSON.parse(htmlSnippets);
-  for (let key in pugSnippets) {
-    let htmlBody = pugSnippets[key].body.join(' ').replace(/\t/g, '');
-    console.log(htmlBody)
-    let pugBody = html2pug(htmlBody, { tabs: true, isFragment: true });
-    pugSnippets[key].body = fixTabs(pugBody.split('\n'));
+function convert (options) {
+  if (options.src && options.dist) {
+    let htmlSnippets = fs.readFileSync(options.src, {encoding: 'utf-8'})
+    let pugSnippets = JSON.parse(htmlSnippets);
+    for (let key in pugSnippets) {
+      let htmlBody = pugSnippets[key].body.join(' ').replace(/\t/g, '');
+      let pugBody = html2pug(htmlBody, { tabs: true, fragment: true });
+      pugSnippets[key].body = fixTabs(pugBody.split('\n'));
+    }
+    fs.writeFileSync(options.dist, JSON.stringify(pugSnippets, null, 2));
+  } else {
+    throw new Error('Missing required arguments');
   }
-  fs.writeFileSync(options.dist, JSON.stringify(pugSnippets, null, 4));
 }
 
 module.exports = {
